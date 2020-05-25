9a3c1338c3d3873487d55035571272698a81e355
Gabriel Jara <gabrielmjara99@gmail.com>
2020-05-25 02:36:14 -0300

    Chapter 14 Complete - Vue Integration

---
+++ vue-integration/src/components/Counter.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <h1>Counter</h1>
+    <h2>{{ this.value }}</h2>
+    <button @click="setValue(1)">+</button>
+    <button @click="setValue(-1)">-</button>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  data() {
+    return {
+      value: this.initialValue || 0
+    };
+  },
+
+  methods: {
+    setValue(delta: number) {
+      this.value += delta;
+    }
+  },
+
+  props: {
+    initialValue: {
+      type: Number
+    }
+  }
+});
+</script>
+
+<style>
+button {
+  font-size: 50px;
+  color: white;
+  background-color: #66eeee;
+  height: 70px;
+  width: 70px;
+  margin: 10px;
+  border-radius: 35px;
+  outline: none;
+}
+</style>
---
+++ vue-integration/src/components/CounterUsingClass.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>Counter Using Class</h1>
+    <h2>{{ value }}</h2>
+    <h2>{{ EvenOrOdd }}</h2>
+    <button @click="setValue(1)">+</button>
+    <button @click="setValue(-1)">-</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from "vue-property-decorator";
+
+@Component
+export default class CounterUsingClass extends Vue {
+  // Required Prop
+  // @Prop(Number) private readonly initialValue!: number;
+  // Optional Prop
+  @Prop(Number) private readonly initialValue?: number;
+
+  private value = this.initialValue || 0;
+
+  public setValue(delta: number) {
+    this.value += delta;
+  }
+
+  public get EvenOrOdd(): string {
+    return this.value % 2 === 0 ? "Even" : "Odd";
+  }
+}
+</script>
+
+<style scoped></style>
